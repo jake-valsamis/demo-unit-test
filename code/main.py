b5460c64f834c634ee6a73515aa60408daf8fccf
Jake Valsamis <jake@codeocean.com>
2024-02-16 14:38:45 +0000

    build app panel, put unit tests in /tests

--- code/main.py
+++ code/main.py
@@ -1,4 +1,10 @@
+import sys
 from circles import circle_area
 
-circle_area = circle_area(5)
-print(circle_area)
+if (len(sys.argv) >= 1):
+    radius = int(sys.argv[1])
+else:
+    radius = 5
+
+circle_area = circle_area(radius)
+print(f"A circle with a radius of {radius} has an area of {circle_area}")
